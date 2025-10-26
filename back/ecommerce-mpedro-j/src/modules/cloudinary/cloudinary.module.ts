a97dee078dbd9dd57a93197b78c060ba216f4f1c
Pedro Jorge Morales <pedrojorgemorales6@gmail.com>
2025-10-26 16:05:04 -0300

    Added Users to cloudinary module context

--- back/ecommerce-mpedro-j/src/modules/cloudinary/cloudinary.module.ts
+++ back/ecommerce-mpedro-j/src/modules/cloudinary/cloudinary.module.ts
@@ -5,9 +5,10 @@ import { CloudinaryController } from './cloudinary.controller';
 import { CloudinaryRepository } from './cloudinary.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Products } from 'src/entities/Products.entity';
+import { Users } from 'src/entities/Users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Products])],
+  imports: [TypeOrmModule.forFeature([Products, Users])],
   providers: [CloudinaryService, CloudinaryConfig, CloudinaryRepository],
   controllers: [CloudinaryController],
 })
